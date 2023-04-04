89e52ab5ad49d5ccbc6cc26ecc9896978e2a6eb7
Noah <njy_roxy@outlook.com>
2023-04-04 18:45:19 +0800

    :hammer: 一步一步改进

--- iot_temp_humi/inc/iot_main.h
+++ iot_temp_humi/inc/iot_main.h
@@ -2,7 +2,7 @@
  * @Author: RoxyKko
  * @Date: 2023-04-04 17:06:27
  * @LastEditors: RoxyKko
- * @LastEditTime: 2023-04-04 17:07:02
+ * @LastEditTime: 2023-04-04 17:43:10
  * @Description: 
  */
 
@@ -19,19 +19,10 @@
 #include <fcntl.h>
 #include <gpiod.h>
 #include <stdint.h>
-#include <time.h>
-#include <sys/stat.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <linux/types.h>
-#include <linux/i2c.h>
-#include <linux/i2c-dev.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <ctype.h>
 #include <syslog.h>
 
 #include "i2c_sht20.h"
+#include "socket_client.h"
 
 # endif
--- iot_temp_humi/src/iot_main.c
+++ iot_temp_humi/src/iot_main.c
@@ -2,7 +2,7 @@
  * @Author: RoxyKko
  * @Date: 2023-03-26 11:22:00
  * @LastEditors: RoxyKko
- * @LastEditTime: 2023-04-04 17:17:28
+ * @LastEditTime: 2023-04-04 18:41:49
  * @Description: iot项目-温湿度检测
  */
 #include "iot_main.h"
@@ -16,7 +16,7 @@
 
 static inline void print_usage(char *progname);
 static inline void print_vision(char *progname);
-int socket_client_init(char *serv_ip, int port);
+
 
 int main(int argc, char **argv)
 {
@@ -154,46 +154,6 @@ int main(int argc, char **argv)
         // 若存在，则将数据写入给服务器
         // 若不存在，则不做任何操作
     }
-}
-
-int socket_client_init(char *serv_ip, int port)
-{
-    int socket_fd;
-    struct sockaddr_in servaddr;
-    int rv = -1;
-
-    // 创建socket并获得socket描述符
-    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    // 创建socket失败
-    if (socket_fd < 0)
-    {
-        printf("Create socket failure: %s\n", strerror(errno));
-        return -1;
-    }
-
-    // 创建socket成功
-    printf("Create socket[%d] successfully\n", socket_fd);
-
-    // 初始化结构体，将空余的8位字节填充为0
-    // 设置参数，connect连接服务器
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family      = AF_INET;                 // IPV4
-    servaddr.sin_port        = htons(port);             // port
-    inet_aton(serv_ip, &servaddr.sin_addr);             // 将点分十进制IP转换为网络字节序
-    rv = connect(socket_fd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-
-    // 连接服务器失败
-    if (rv < 0)
-    {
-        printf("Connect to server[%s:%d] failure : %s\n",
-               serv_ip, port, strerror(errno));
-        return -2;
-    }
-    // 连接服务器成功,打印服务器IP地址和端口号
-    printf("Connect to server[%s:%d] successfully!\n", serv_ip, port);
-
-    // 若链接成功则返回socket描述符
-    return socket_fd;
 }
 
 /**
