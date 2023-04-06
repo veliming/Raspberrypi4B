07e01f83da8f2e9c3941355fbbc9f1c654897254
Noah <njy_roxy@outlook.com>
2023-04-06 15:19:02 +0800

    :dart: 若是逃避和前进之中，你选择了前进

---
+++ iot_temp_humi/inc/database.h
@@ -0,0 +1,30 @@
+/*** 
+ * @Author: RoxyKko
+ * @Date: 2023-04-05 19:24:03
+ * @LastEditors: RoxyKko
+ * @LastEditTime: 2023-04-05 22:58:36
+ * @Description: sqlite的使用
+ */
+
+#ifndef __DATABASE_H__
+#define __DATABASE_H__
+
+#include "iot_main.h"
+#include "packinfo.h"
+
+int database_init(char *dbname, sqlite3 *db);
+
+int database_close(char *dbname, sqlite3 *db);
+
+int database_create_table(char *dbname, sqlite3 *db);
+
+int database_insert_data(char *dbname, sqlite3 *db, packinfo_t *pack_info);
+
+int database_select_data(char *dbname, sqlite3 *db, packinfo_t *pack_info);
+
+int database_delete_data(char *dbname, sqlite3 *db, packinfo_t *pack_info);
+
+
+
+
+# endif
